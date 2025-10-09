91960dc8833d64e232078a4d079ebba488cd0f0f
CyberByteX-7 <149484548+CyberByteX-7@users.noreply.github.com>
2025-10-09 23:58:33 +0530

    Add files via upload

---
+++ wat.ino
@@ -0,0 +1,21 @@
+const int waterSensorPin = A0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Water Level Sensor - Ready");
+}
+
+void loop() {
+  int sensorValue = analogRead(waterSensorPin);
+  Serial.print("Raw Sensor Value: ");
+  Serial.print(sensorValue);
+
+  // Map the sensor value to a percentage (0-100%)
+  int waterLevelPercent = map(sensorValue, 0, 600, 0, 100);
+
+  Serial.print("  |  Water Level: ");
+  Serial.print(waterLevelPercent);
+  Serial.println("%");
+
+  delay(500);
+}
